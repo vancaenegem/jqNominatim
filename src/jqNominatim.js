1f1187ad296324cb9c0fa2fac611d4cda3ceb960
VAN CAENEGEM STEPHANE (CPAM INDRE-ET-LOIRE) <VANCAENEGEM-02802@cnamts.local>
2020-01-03 16:53:12 +0100

    Creation du fichier a partir du "boilerplate jQuery"

---
+++ src/jqNominatim.js
@@ -0,0 +1,45 @@
+;(function($) {
+
+    $.jqNominatim = function(element, options) {
+
+        var defaults = {
+            foo: 'bar',
+            onFoo: function() {}
+        }
+
+        var plugin = this;
+
+        plugin.settings = {}
+
+        var $element = $(element),
+             element = element;
+
+        plugin.init = function() {
+            plugin.settings = $.extend({}, defaults, options);
+            // code goes here
+        }
+
+        plugin.foo_public_method = function() {
+            // code goes here
+        }
+
+        var foo_private_method = function() {
+            // code goes here
+        }
+
+        plugin.init();
+
+    }
+
+    $.fn.jqNominatim = function(options) {
+
+        return this.each(function() {
+            if (undefined == $(this).data('jqNominatim')) {
+                var plugin = new $.jqNominatim(this, options);
+                $(this).data('jqNominatim', plugin);
+            }
+        });
+
+    }
+
+})(jQuery);
